31d251530291491d49709254ed629ffbd67e1920
James West <jwest.126@gmail.com>
2013-07-08 14:01:08 -0400

    Moved database test code and removed test observer class

--- src/java/mil/afrl/discoverylab/sate13/ripplebroker/BrokerContextListener.java
+++ src/java/mil/afrl/discoverylab/sate13/ripplebroker/BrokerContextListener.java
@@ -24,6 +24,7 @@ import mil.afrl.discoverylab.sate13.ripplebroker.db.DatabaseHelper;
 import mil.afrl.discoverylab.sate13.ripplebroker.db.DatabaseMessageListener;
 import mil.afrl.discoverylab.sate13.ripplebroker.util.Config;
 import mil.afrl.discoverylab.sate13.ripplebroker.util.Reference;
+import mil.afrl.discoverylab.sate13.ripplebroker.util.Reference.TableColumns;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -49,9 +50,7 @@ public class BrokerContextListener implements ServletContextListener {
         try {
             // listen on anylocal address (:: or 0:0:0:0:0:0:0:0)
             task = new UDPListener(Inet6Address.getByAddress(new byte[16]), LISTEN_PORT);
-            //task.addObserver(new testObserver());
-            if(Config.AUTO_DATABASE_INSERT)
-            {
+            if (Config.AUTO_DATABASE_INSERT) {
                 task.addObserver(new DatabaseMessageListener());
             }
         } catch (UnknownHostException ex) {
@@ -113,114 +112,37 @@ public class BrokerContextListener implements ServletContextListener {
     private void initDatabase(ServletContext servletContext) {
         try {
             DatabaseHelper db = DatabaseHelper.getInstance(servletContext);
-            // TODO: remove test code
-//            try {
-//                log.debug(db.patientExists(Inet6Address.getByName("aaaa:0000:0000:0000:0000:0000:1234:0001")));
-//            } catch (UnknownHostException ex) {
-//                log.error("Error", ex);
-//            }
-            
-//            List<Entry<Reference.tableColumns, String>> entries = new ArrayList<Entry<Reference.tableColumns, String>>();
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.FIRST_NAME, "John"));
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.LAST_NAME, "Doe"));
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.IP_ADDR, Inet6Address.getByName("aaaa:0000:0000:0000:0000:0000:1234:0001").getHostAddress()));
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.SEX, "Male"));
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.DOB, Reference.datetimeFormat.format(new Date())));
-//            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.TYPE, "Unknown"));
-//            db.insertRow(Reference.TABLE_NAMES.PATIENT, entries);
-//            db.insertRow(Reference.TABLE_NAMES.PATIENT, entries);
         } catch (ClassNotFoundException ex) {
             log.error("Error initializing database", ex);
-        } //catch (UnknownHostException ex) {
-//            log.error("Unknown host", ex);
-//        }
-    }
-
-    private class testObserver implements Observer {
-
-        @Override
-        public void update(Observable o, Object arg) {
-            if (o instanceof UDPListener) {
-                UDPListenerObservation observation = (UDPListenerObservation) arg;
-                log.debug("Update called.");
-                this.parseRippleMessage(observation.getMessage());
-            }
-        }
-
-        private void parseRippleMessage(byte[] message) {
-            int overflowCount = (message[0] & 0xff);
-            long timestamp = 0;
-            int pulse = 0;
-            int bloodOx = 0;
-            int temperature = 0;
-
-            for (int i = 1; i < 4; i++) {
-                timestamp |= (message[i] & 0xff);
-                timestamp = (timestamp << 8);
-            }
-            timestamp |= (message[4] & 0xff);
-
-            log.debug("Overflow count: " + overflowCount);
-            log.debug("Timestamp: " + timestamp);
-
-            int type = (message[5] & 0xff);
-
-            if (type == Reference.SENSOR_TYPES.SENSOR_PULSE_OX.getValue()) {
-                // got pulse and blood oxygen message
-                int numPulseOxSamples = (message[6] & 0x00ff);
-
-                // heart rate
-                //pulse = ((uint16_t)b[6] << (1*8));
-                //pulse |= ((uint16_t)b[7] & 0x00ff);
-
-                // TODO: iterate through multiple samples(if provided)
-                pulse |= (message[7] & 0xff);
-                pulse = (pulse << 8) | (message[8] & 0xff);
-
-
-                // blood oxygen
-                bloodOx = (message[9] & 0xff);
-
-                log.debug("Reported pulse and blood oxygen:");
-                log.debug("Num samples: " + numPulseOxSamples);
-
-                log.debug("Pulse (BPM): " + pulse);
-                log.debug("Blood oxygen: " + bloodOx);
-            } else if (type == Reference.SENSOR_TYPES.SENSOR_TEMPERATURE.getValue()) {
-                // got temperature reading message
-                int numTemperatureSamples = (message[6] & 0x00ff);
-
-                // TODO: iterate through multiple samples(if provided)
-                temperature = (message[7] & 0x00ff);
-
-                log.debug("Reported temperature:");
-                log.debug("Num samples: " + numTemperatureSamples);
-                log.debug("Temperature: " + temperature);
-
-            } else if (type == Reference.SENSOR_TYPES.SENSOR_ECG.getValue()) {
-                // got ecg reading message
-                int numEcgSamples = (message[6] & 0x00ff);
-
-                int sampleOffsets = (message[7] & 0xff);
-                int[] data = new int[numEcgSamples];
-
-                log.debug("Reported ECG:");
-                log.debug("Offset is " + sampleOffsets + " ms");
-
-                for (int i = 0, buf_count = 8; i < numEcgSamples; i++, buf_count += 2) {
-                    data[i] |= (message[buf_count] & 0xff);
-                    data[i] = (data[i] << 8) | (message[buf_count + 1] & 0xff);
-
-                    log.debug("Data: " + data[i]);
-                }
-
-
-
-            } else {
-                log.error("Unknown message! Type: " + message[5]);
-            }
-
-
         }
     }
+
+    private void testDatabase() {
+        // test code
+        try {
+            DatabaseHelper db = DatabaseHelper.getInstance(null);
+            try {
+                log.debug(db.patientExists(Inet6Address.getByName("aaaa:0000:0000:0000:0000:0000:1234:0001")));
+            } catch (UnknownHostException ex) {
+                log.error("Error", ex);
+            }
+
+            List<Entry<Reference.TableColumns, String>> entries = new ArrayList<Entry<Reference.TableColumns, String>>();
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.FIRST_NAME, "John"));
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.LAST_NAME, "Doe"));
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.IP_ADDR, Inet6Address.getByName("aaaa:0000:0000:0000:0000:0000:1234:0001").getHostAddress()));
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.SEX, "Male"));
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.DOB, Reference.datetimeFormat.format(new Date())));
+            entries.add(new SimpleEntry(Reference.PATIENT_TABLE_COLUMNS.TYPE, "Unknown"));
+            db.insertRow(Reference.TABLE_NAMES.PATIENT, entries);
+            db.insertRow(Reference.TABLE_NAMES.PATIENT, entries);
+
+        } catch (UnknownHostException ex) {
+            log.error("Unknown host", ex);
+        } catch (ClassNotFoundException ex) {
+            log.error("Class not found database helper", ex);
+        }
+    }
+
+   
 }
